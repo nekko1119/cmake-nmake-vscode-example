f179958f6b9055cfbafebc227ca30d8f0f237191
nekko1119 <taku50501119@gmail.com>
2020-01-31 05:42:19 +0900

    update clang-tidy checks

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 //-----------------------------------------------------------------------------
 #include <d3d9.h>
+
+#include <array>
 #pragma warning(disable : 4996) // disable deprecated warning
 #include <strsafe.h>
 #pragma warning(default : 4996)
@@ -23,9 +25,9 @@
 //-----------------------------------------------------------------------------
 // Global variables
 //-----------------------------------------------------------------------------
-LPDIRECT3D9 g_pD3D = nullptr; // Used to create the D3DDevice
-LPDIRECT3DDEVICE9 g_pd3dDevice = nullptr; // Our rendering device
-LPDIRECT3DVERTEXBUFFER9 g_pVB = nullptr; // Buffer to hold vertices
+LPDIRECT3D9 g_p_d3_d = nullptr; // Used to create the D3DDevice
+LPDIRECT3DDEVICE9 g_pd3d_device = nullptr; // Our rendering device
+LPDIRECT3DVERTEXBUFFER9 g_p_vb = nullptr; // Buffer to hold vertices
 
 // A structure for our custom vertex type
 struct CUSTOMVERTEX {
@@ -34,16 +36,16 @@ struct CUSTOMVERTEX {
 };
 
 // Our custom FVF, which describes our custom vertex structure
-#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
+constexpr auto D3DFVF_CUSTOMVERTEX = (D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
 
 //-----------------------------------------------------------------------------
 // Name: InitD3D()
 // Desc: Initializes Direct3D
 //-----------------------------------------------------------------------------
 HRESULT
-InitD3D(HWND hWnd) {
+init_d3_d(HWND hWnd) {
   // Create the D3D object.
-  if (nullptr == (g_pD3D = Direct3DCreate9(D3D_SDK_VERSION))) {
+  if (nullptr == (g_p_d3_d = Direct3DCreate9(D3D_SDK_VERSION))) {
     return E_FAIL;
   }
 
@@ -55,8 +57,8 @@ InitD3D(HWND hWnd) {
   d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
 
   // Create the D3DDevice
-  if (FAILED(g_pD3D->CreateDevice(
-        D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_pd3dDevice))) {
+  if (FAILED(g_p_d3_d->CreateDevice(
+        D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_pd3d_device))) {
     return E_FAIL;
   }
 
@@ -75,49 +77,51 @@ InitD3D(HWND hWnd) {
 //       cards to process them in hardware, resulting in a dramatic
 //       performance gain.
 //-----------------------------------------------------------------------------
-HRESULT InitVB() {
+HRESULT init_vb() {
   // Initialize three vertices for rendering a triangle
-  CUSTOMVERTEX vertices[] = {
+  std::array<CUSTOMVERTEX, 3> vertices = {{
     {
-      150.0f,
-      50.0f,
-      0.5f,
-      1.0f,
+      150.0F,
+      50.0F,
+      0.5F,
+      1.0F,
       0xffff0000,
     }, // x, y, z, rhw, color
     {
-      250.0f,
-      250.0f,
-      0.5f,
-      1.0f,
+      250.0F,
+      250.0F,
+      0.5F,
+      1.0F,
       0xff00ff00,
     },
     {
-      50.0f,
-      250.0f,
-      0.5f,
-      1.0f,
+      50.0F,
+      250.0F,
+      0.5F,
+      1.0F,
       0xff00ffff,
     },
-  };
+  }};
 
   // Create the vertex buffer. Here we are allocating enough memory
   // (from the default pool) to hold all our 3 custom vertices. We also
   // specify the FVF, so the vertex buffer knows what data it contains.
-  if (FAILED(g_pd3dDevice->CreateVertexBuffer(
-        3 * sizeof(CUSTOMVERTEX), 0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVB, nullptr))) {
+  auto hr = g_pd3d_device->CreateVertexBuffer(
+    3 * sizeof(CUSTOMVERTEX), /*Usage=*/0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_p_vb, /*pSharedHandle=*/nullptr);
+  if (FAILED(hr)) {
     return E_FAIL;
   }
 
   // Now we fill the vertex buffer. To do this, we need to Lock() the VB to
   // gain access to the vertices. This mechanism is required becuase vertex
   // buffers may be in device memory.
-  VOID* pVertices;
-  if (FAILED(g_pVB->Lock(0, sizeof(vertices), (void**) &pVertices, 0))) {
+  VOID* p_vertices;
+  hr = g_p_vb->Lock(/*OffsetToLock=*/0, sizeof(vertices), &p_vertices, /*Flags=*/0);
+  if (FAILED(hr)) {
     return E_FAIL;
   }
-  memcpy(pVertices, &vertices, sizeof(vertices));
-  g_pVB->Unlock();
+  memcpy(p_vertices, vertices.data(), sizeof(vertices));
+  g_p_vb->Unlock();
 
   return S_OK;
 }
@@ -126,17 +130,17 @@ HRESULT InitVB() {
 // Name: Cleanup()
 // Desc: Releases all previously initialized objects
 //-----------------------------------------------------------------------------
-VOID Cleanup() {
-  if (g_pVB != nullptr) {
-    g_pVB->Release();
+VOID cleanup() {
+  if (g_p_vb != nullptr) {
+    g_p_vb->Release();
   }
 
-  if (g_pd3dDevice != nullptr) {
-    g_pd3dDevice->Release();
+  if (g_pd3d_device != nullptr) {
+    g_pd3d_device->Release();
   }
 
-  if (g_pD3D != nullptr) {
-    g_pD3D->Release();
+  if (g_p_d3_d != nullptr) {
+    g_p_d3_d->Release();
   }
 }
 
@@ -144,12 +148,13 @@ VOID Cleanup() {
 // Name: Render()
 // Desc: Draws the scene
 //-----------------------------------------------------------------------------
-VOID Render() {
+VOID render() {
   // Clear the backbuffer to a blue color
-  g_pd3dDevice->Clear(0, nullptr, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
+  g_pd3d_device->Clear(
+    /*Count=*/0, /*pRects=*/nullptr, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 255), /*Z=*/1.0F, /*Stencil=*/0);
 
   // Begin the scene
-  if (SUCCEEDED(g_pd3dDevice->BeginScene())) {
+  if (SUCCEEDED(g_pd3d_device->BeginScene())) {
     // Draw the triangles in the vertex buffer. This is broken into a few
     // steps. We are passing the vertices down a "stream", so first we need
     // to specify the source of that stream, which is our vertex buffer. Then
@@ -158,27 +163,29 @@ VOID Render() {
     // just the FVF, so that D3D knows what type of vertices we are dealing
     // with. Finally, we call DrawPrimitive() which does the actual rendering
     // of our geometry (in this case, just one triangle).
-    g_pd3dDevice->SetStreamSource(0, g_pVB, 0, sizeof(CUSTOMVERTEX));
-    g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
-    g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
+    g_pd3d_device->SetStreamSource(/*StreamNumber=*/0, g_p_vb, /*OffsetInBytes=*/0, sizeof(CUSTOMVERTEX));
+    g_pd3d_device->SetFVF(D3DFVF_CUSTOMVERTEX);
+    g_pd3d_device->DrawPrimitive(D3DPT_TRIANGLELIST, /*StartVertex=*/0, /*PrimitiveCount=*/1);
 
     // End the scene
-    g_pd3dDevice->EndScene();
+    g_pd3d_device->EndScene();
   }
 
   // Present the backbuffer contents to the display
-  g_pd3dDevice->Present(nullptr, nullptr, nullptr, nullptr);
+  g_pd3d_device->Present(
+    /*pSourceRect=*/nullptr, /*pDestRect=*/nullptr, /*hDestWindowOverride=*/nullptr, /*pDirtyRegion=*/nullptr);
 }
 
 //-----------------------------------------------------------------------------
 // Name: MsgProc()
 // Desc: The window's message handler
 //-----------------------------------------------------------------------------
-LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+LRESULT WINAPI msg_proc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+  // NOLINTNEXTLINE(hicpp-multiway-paths-covered)
   switch (msg) {
     case WM_DESTROY:
-      Cleanup();
-      PostQuitMessage(0);
+      cleanup();
+      PostQuitMessage(/*nExitCode=*/0);
       return 0;
   }
 
@@ -189,16 +196,17 @@ LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 // Name: wWinMain()
 // Desc: The application's entry point
 //-----------------------------------------------------------------------------
+// NOLINTNEXTLINE(readability-identifier-naming, readability-inconsistent-declaration-parameter-name)
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE /*unused*/, LPSTR /*unused*/, int /*unused*/) {
   UNREFERENCED_PARAMETER(hInst);
 
   // Register the window class
   WNDCLASSEX wc = {sizeof(WNDCLASSEX),
                    CS_CLASSDC,
-                   MsgProc,
+                   msg_proc,
                    0L,
                    0L,
-                   GetModuleHandle(nullptr),
+                   GetModuleHandle(/*lpModuleName=*/nullptr),
                    LoadIcon(hInst, TEXT("MAINICON")),
                    nullptr,
                    nullptr,
@@ -208,7 +216,7 @@ INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE /*unused*/, LPSTR /*unused*/, int 
   RegisterClassEx(&wc);
 
   // Create the application's window
-  HWND hWnd = CreateWindow(
+  HWND h_wnd = CreateWindow(
     TEXT("D3D Tutorial"),
     TEXT("D3D Tutorial 02: Vertices"),
     WS_OVERLAPPEDWINDOW,
@@ -222,22 +230,22 @@ INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE /*unused*/, LPSTR /*unused*/, int 
     nullptr);
 
   // Initialize Direct3D
-  if (SUCCEEDED(InitD3D(hWnd))) {
+  if (SUCCEEDED(init_d3_d(h_wnd))) {
     // Create the vertex buffer
-    if (SUCCEEDED(InitVB())) {
+    if (SUCCEEDED(init_vb())) {
       // Show the window
-      ShowWindow(hWnd, SW_SHOWDEFAULT);
-      UpdateWindow(hWnd);
+      ShowWindow(h_wnd, SW_SHOWDEFAULT);
+      UpdateWindow(h_wnd);
 
       // Enter the message loop
       MSG msg;
       ZeroMemory(&msg, sizeof(msg));
       while (msg.message != WM_QUIT) {
-        if (PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE)) {
+        if (PeekMessage(&msg, /*hWnd=*/nullptr, /*wMsgFilterMin=*/0U, /*wMsgFilterMax=*/0U, PM_REMOVE)) {
           TranslateMessage(&msg);
           DispatchMessage(&msg);
         } else {
-          Render();
+          render();
         }
       }
     }
